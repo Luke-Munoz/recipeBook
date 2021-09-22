d5976bfb6063714f800b79d2040994e0c145a89b
unknown <jtjackson325@gmail.com>
2021-09-21 18:51:33 -0600

    made the password, email address unique and the user is required to enter both and both have to match a regex expression

--- models/User.js
+++ models/User.js
@@ -1,21 +1,44 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
-    username: {
-        type: String
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: [true, 'Please enter a username of your choice'],
+            trim: true
+        },
+        email: {
+            type: String,
+            required: [true, 'Please enter an email'],
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\wa-z\.-]+)\.([a-z\.]{2,8})$/, 'Please enter an email address that meets specifications']
+        },
+        password: {
+            type: String,
+            required: [true, 'You must choose a password'],
+            unique: true,
+            match: [/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, 'Please choose a password that has at least one letter, one number, one special character and is as least eight characters long']
+        },
+        comments: [
+            {
+                // type: Schema.Types.ObjectId,
+                // ref: 'Recipe'
+            }
+        ]
     },
-    email: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    comments: [
-        {
-            type: String
-        }
-    ]
-});
+    {
+        // toJSON: {
+            //virtuals: true,
+        // },
+        // id: false
+    }
+);
+
+// placeholder for counting how many comments our recipe module will have
+// UserSchema.vitual('commentCount').get(function() {
+//    return this.comments.length
+// })
 
 const User = model('User', UserSchema)
 
