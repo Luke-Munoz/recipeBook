64668e0e27119e7eef0e73904e11f49ca25e41e3
unknown <jtjackson325@gmail.com>
2021-09-23 13:16:53 -0600

    should be able to deploy to heroku

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost/recipebook', {
-    // useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/recipebook', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
