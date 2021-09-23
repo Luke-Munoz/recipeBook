2a987015b1c4ed1f4e5259dec016946aad2395f9
Luke <munozluke1@gmail.com>
2021-09-23 15:40:21 -0600

    making controllers for commenting

---
+++ comments-controller.js
@@ -0,0 +1,26 @@
+const { Comment, Recipe } = require("../models");
+
+
+const commentController = {
+
+    addComment({ params, body }, res) {
+        console.log(body);
+        Comment.create(body)
+            .then(({ _id }) => {
+                return Recipe.findOneAndUpdate({ _id: params.recipeId }, { $push: { comments: _id } }, { new: true });
+            })
+            .then(dbRecipesData => {
+                if (!dbRecipeData) {
+                    res.status(404).json({ message: 'No recipe found with this id!' });
+                    return;
+                }
+                res.json(dbRecipeData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate({ _id: params.commentId }, )
+    }
+
+}
