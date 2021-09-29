5c7dca8fa6ebc5bc24ccf6089ae2cb82132c700d
unknown <jtjackson325@gmail.com>
2021-09-29 10:56:27 -0600

    got the seed file constructed

--- seed/seed.js
+++ seed/seed.js
@@ -17,85 +17,90 @@ seeder.connect('mongodb://localhost/recipebook', function() {
     });
 });
 // Data array containing seed data - documents organized by Model
-const data = [{
-    User: [{
-            username: "TheRealMario",
-            email: "MarioIsBetter@gmail.com",
-            password: "PeachIsmyQueen1!",
-        },
-        {
-            username: "TheRealMario",
-            email: "MarioIsBetter@gmail.com",
-            password: "PeachIsmyQueen1!",
-        },
-        {
-            username: "TheRealMario",
-            email: "MarioIsBetter@gmail.com",
-            password: "PeachIsmyQueen1!",
-        },
-        {
-            username: "TheRealMario",
-            email: "MarioIsBetter@gmail.com",
-            password: "PeachIsmyQueen1!",
-        },
-    ],
-    Recipes: [{
-            recipeTitle: "Yummy Food",
-            recipeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-            createdBy: "Mario",
-            createdAt: {
-                type: Date,
-                default: Date.now,
-                get: (createdAtVal) => dateFormat(createdAtVal),
+const data = [
+    {
+        'model': 'User',
+        'documents': [
+            {
+                username: "TheRealMario",
+                email: "MarioIsBetter@gmail.com",
+                password: "PeachIsmyQueen1!",
             },
-            comments: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-        },
-        {
-            recipeTitle: "Steak",
-            recipeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-            createdBy: "Yoda",
-            createdAt: {
-                type: Date,
-                default: Date.now,
-                get: (createdAtVal) => dateFormat(createdAtVal),
+            {
+                username: "TheRealMario",
+                email: "MarioIsBetter@gmail.com",
+                password: "PeachIsmyQueen1!",
             },
-            comments: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-        },
-        {
-            recipeTitle: "Soup",
-            recipeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-            createdBy: "Bowser",
-            createdAt: {
-                type: Date,
-                default: Date.now,
-                get: (createdAtVal) => dateFormat(createdAtVal),
+            {
+                username: "TheRealMario",
+                email: "MarioIsBetter@gmail.com",
+                password: "PeachIsmyQueen1!",
             },
-            comments: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-        },
-        {
-            recipeTitle: "Sandwhich",
-            recipeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-            createdBy: "Pikachu",
-            createdAt: {
-                type: Date,
-                default: Date.now,
-                get: (createdAtVal) => dateFormat(createdAtVal),
-            },
-            comments: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-        },
-        {
-            recipeTitle: "Water",
-            recipeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-            createdBy: "Jeff",
-            createdAt: {
-                type: Date,
-                default: Date.now,
-                get: (createdAtVal) => dateFormat(createdAtVal),
-            },
-            comments: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-        },
-    ],
-}, ];
+            {
+                username: "TheRealMario",
+                email: "MarioIsBetter@gmail.com",
+                password: "PeachIsmyQueen1!",
+            }
+        ],
 
-
+        'model': 'Recipes',
+        'documents': [
+            {
+                recipeTitle: "Yummy Food",
+                recipeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                createdBy: "Mario",
+                createdAt: {
+                    type: Date,
+                    default: Date.now,
+                    get: (createdAtVal) => dateFormat(createdAtVal),
+                },
+                comments: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            },
+            {
+                recipeTitle: "Steak",
+                recipeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                createdBy: "Yoda",
+                createdAt: {
+                    type: Date,
+                    default: Date.now,
+                    get: (createdAtVal) => dateFormat(createdAtVal),
+                },
+                comments: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            },
+            {
+                recipeTitle: "Soup",
+                recipeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                createdBy: "Bowser",
+                createdAt: {
+                    type: Date,
+                    default: Date.now,
+                    get: (createdAtVal) => dateFormat(createdAtVal),
+                },
+                comments: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            },
+            {
+                recipeTitle: "Sandwhich",
+                recipeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                createdBy: "Pikachu",
+                createdAt: {
+                    type: Date,
+                    default: Date.now,
+                    get: (createdAtVal) => dateFormat(createdAtVal),
+                },
+                comments: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            },
+            {
+                recipeTitle: "Water",
+                recipeText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                createdBy: "Jeff",
+                createdAt: {
+                    type: Date,
+                    default: Date.now,
+                    get: (createdAtVal) => dateFormat(createdAtVal),
+                },
+                comments: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            },
+        ]
+    }
+]
 
