9c6da8ddddd042ca20cae6fa3d8eaf2fbc19cf30
Jacekhacking <82614083+Jacekhacking@users.noreply.github.com>
2021-10-03 21:14:58 -0600

    devops

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const MongoStore = require('connect-mongo');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/recipebook";
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/recipebook";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/recipebook"
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
