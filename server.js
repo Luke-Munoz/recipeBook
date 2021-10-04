7a2805a862bcbcd5501e23f3a88c0a118ddf3d8c
Jacekhacking <82614083+Jacekhacking@users.noreply.github.com>
2021-10-03 23:44:19 -0600

    devops

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ app.use(require('./routes'));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'client/build/index.html')); })
+    // app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'client/build/index.html')); })
 
 } else {
     
     // dev mode
-    app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'client/public/index.html')); })
+    // app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'client/public/index.html')); })
 
 }
 
